01d947a6fc9a6efcd93f9cbf716723a08809f9f1
Djesfin1973 <127182861+Djesfin1973@users.noreply.github.com>
2023-03-08 18:44:11 +0300

    Update калькулятор.py

--- калькулятор.py
+++ калькулятор.py
@@ -1,15 +1,29 @@
-Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import math
-... 
-... def calculate_distance_between_balloons(distance, altitude):
-...     earth_radius = 6371 # радиус земли в км
-...     distance += earth_radius # учитываем расстояние до центра Земли
-...     altitude += earth_radius # учитываем высоту над уровнем моря
-...     angle = 2 * math.asin(distance / (2 * (earth_radius + altitude))) # вычисляем угол в радианах
-...     distance_between_balloons = 2 * (earth_radius + altitude) * math.sin(angle / 2) # вычисляем расстояние между шарами
-...     return distance_between_balloons
-... 
-... # пример использования функции для расчета расстояния между шарами на расстоянии 1000 км друг от друга на высоте 5 км
-... result = calculate_distance_between_balloons(1000, 5)
-... print("Расстояние между шарами:", result, "км")
+import math
+
+def distance(lat1, lon1, lat2, lon2):
+    R = 6371  # Радиус Земли в километрах
+    dLat = math.radians(lat2-lat1)
+    dLon = math.radians(lon2-lon1)
+    a = math.sin(dLat/2) * math.sin(dLat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dLon/2) * math.sin(dLon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    dist = R * c  # Расстояние в километрах
+    return dist
+
+def elevation(lat1, lon1, lat2, lon2):
+    dist = distance(lat1, lon1, lat2, lon2)
+    h1 = 0  # Высота над уровнем моря первой точки
+    h2 = 0  # Высота над уровнем моря второй точки
+    # Здесь нужно получить данные о высотах точек с помощью какого-либо API или другого источника данных
+    ele = abs(h2 - h1)  # Высота разницы между точками
+    return ele
+
+def line_of_sight(lat1, lon1, h1, lat2, lon2, h2):
+    R = 6371  # Радиус Земли в километрах
+    earth_radius = 6371.01  # Радиус Земли с поправкой на геоид
+    d = distance(lat1, lon1, lat2, lon2)
+    a = math.acos((R+h1) / (R+h2))
+    b = math.acos((R+h1) / (R+h2))
+    c = math.acos(math.cos(a) * math.cos(b) + math.sin(a) * math.sin(b) * math.cos(d/earth_radius))
+    los_distance = R * c  # Расстояние прямой видимости в километрах
+    return los_distance
